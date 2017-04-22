abe72c283e8bb9da4b6374a5a60adec99815ac20
Brayden <brayden.h@mynaturalmarket.com>
2017-04-21 23:45:02 -0600

    able to sort

--- src/app/view-email-builder/view-email-builder.component.ts
+++ src/app/view-email-builder/view-email-builder.component.ts
@@ -7,11 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewEmailBuilderComponent implements OnInit {
   simpleDrop: any = null;
-
+  singleColumnText: Object = { id: 1, msg: 'Hello', html: "<table><tr align='center'><td>Text</td></tr></table>" };
+  doubleColumnText: Object = { id: 1, msg: 'Hello', html: "<table><tr align='center'><td>Text 1</td><td>Text 2</td></tr></table>" };
+  receivedData: Array<any> = [];
+  
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+
+
+  transferDataSuccess($event: any) {
+    this.receivedData.push($event);
   }
 
 }
