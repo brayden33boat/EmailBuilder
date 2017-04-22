52ae47ab65d75b4874125941c7f8c9c61e823971
Brayden <brayden.h@mynaturalmarket.com>
2017-04-22 13:25:23 -0600

    stuff

--- src/app/view-email-builder/widgets.service.ts
+++ src/app/view-email-builder/widgets.service.ts
@@ -15,7 +15,7 @@ export class WidgetsService {
     widgets.push(new Widget(3,"fa fa-align-center fa-6", "<table style='width:100%'><tr align='center'><td>{{text1}}</td><td>{{text2}}</td><td>{{text3}}</td></tr></table>", [{ 'selector': 'text1', 'value': 'hello1' }, { 'selector': 'text2', 'value': 'hello2' }, { 'selector': 'text3', 'value': 'hello3' }]));
 
     widgets.push(new Widget(4,"fa fa-header fa-6", "<table style='width:100%'><tr align='center'><td><h1>{{title}}</h1></td></tr></table>", [{ 'selector': 'title', 'value': 'Title 1' }]));
-    widgets.push(new Widget(4,"fa fa-header fa-4", "<table style='width:100%'><tr align='center'><td><h3>{{title}}</h3></td></tr></table>", [{ 'selector': 'title', 'value': 'Title 1' }]));
+    widgets.push(new Widget(4,"fa fa-header fa-6", "<table style='width:100%'><tr align='center'><td><h3>{{title}}</h3></td></tr></table>", [{ 'selector': 'title', 'value': 'Title 1' }]));
 
     return widgets;
   }
