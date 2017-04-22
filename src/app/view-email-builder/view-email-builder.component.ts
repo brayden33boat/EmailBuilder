effcec69f6bc54c6623d244464deeecdc7b2528a
Brayden <brayden.h@mynaturalmarket.com>
2017-04-22 00:19:20 -0600

    styling for when to html

--- src/app/view-email-builder/view-email-builder.component.ts
+++ src/app/view-email-builder/view-email-builder.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewEmailBuilderComponent implements OnInit {
   simpleDrop: any = null;
-  singleColumnText: Object = { id: 1, msg: 'Hello', html: "<table><tr align='center'><td>Text</td></tr></table>" };
-  doubleColumnText: Object = { id: 1, msg: 'Hello', html: "<table><tr align='center'><td>Text 1</td><td>Text 2</td></tr></table>" };
+  singleColumnText: Object = { id: 1, msg: 'Hello', html: "<table style='width:100%'><tr align='center'><td>Text</td></tr></table>" };
+  doubleColumnText: Object = { id: 2, msg: 'Hello', html: "<table style='width:100%'><tr align='center'><td>Text 1</td><td>Text 2</td></tr></table>" };
+  
   receivedData: Array<any> = [];
+
+  html:String;
+  showHtml:Boolean = false;
   
 
   constructor() { }
@@ -17,10 +21,37 @@ export class ViewEmailBuilderComponent implements OnInit {
   ngOnInit() {
   }
 
+  reset(){
+    this.receivedData=[];
+  }
+
+  toggleHtml(){
+    this.showHtml=!this.showHtml;
+  }
+
+  toHtml(){
+    var html = '<table style="width:100%"><tbody>';
+
+    for (var i = 0; i < this.receivedData.length; i++) {
+      html += "<tr><td>";
+      html += this.receivedData[i].dragData.html;
+      html += "</td></tr>";
+    }
+
+    html += '</tbody></table>';
+
+    this.html = html;
+  }
+
+  editItem(){
+    console.log('Edit Item');
+  }
+
 
 
   transferDataSuccess($event: any) {
     this.receivedData.push($event);
+    this.toHtml();
   }
 
 }
