339a7c25023ffd0001aa121e423b0c3357caa4ae
Brayden <brayden.h@mynaturalmarket.com>
2017-04-22 14:41:01 -0600

    img width 100%

--- src/app/view-email-builder/widgets.service.ts
+++ src/app/view-email-builder/widgets.service.ts
@@ -17,8 +17,8 @@ export class WidgetsService {
     widgets.push(new Widget(4,2,"fa fa-header fa-6", "<table style='width:100%'><tr align='center'><td><h1>{{title}}</h1></td></tr></table>", [{ 'selector': 'title', 'value': 'Title 1' }]));
     widgets.push(new Widget(5,2,"fa fa-header fa-6", "<table style='width:100%'><tr align='center'><td><h3>{{title}}</h3></td></tr></table>", [{ 'selector': 'title', 'value': 'Title 1' }]));
 
-    widgets.push(new Widget(6,3,"fa fa-address-card-o fa-flip-horizontal fa-6", "<table style='width:100%'><tr align='center'><td style='width:50%'><p>{{text}}</p></td><td style='width:50%'><img src='{{img}}' /></td></tr></table>", [{ 'selector': 'text', 'value': 'text' },{'selector':'img','value':'http://www.lijsoccer.com/images/soccerBall.png'}]));
-    widgets.push(new Widget(7,3,"fa fa-address-card-o fa-6", "<table style='width:100%'><tr align='center'><td style='width:50%'><img src='{{img}}' /></td><td style='width:50%'><p>{{text}}</p></td></tr></table>", [{ 'selector': 'text', 'value': 'text' },{'selector':'img','value':'http://www.lijsoccer.com/images/soccerBall.png'}]));
+    widgets.push(new Widget(6,3,"fa fa-address-card-o fa-flip-horizontal fa-6", "<table style='width:100%'><tr align='center'><td style='width:50%'><p>{{text}}</p></td><td style='width:50%'><img width='100%' src='{{img}}' /></td></tr></table>", [{ 'selector': 'text', 'value': 'text' },{'selector':'img','value':'http://www.lijsoccer.com/images/soccerBall.png'}]));
+    widgets.push(new Widget(7,3,"fa fa-address-card-o fa-6", "<table style='width:100%'><tr align='center'><td style='width:50%'><img width='100%' src='{{img}}' /></td><td style='width:50%'><p>{{text}}</p></td></tr></table>", [{ 'selector': 'text', 'value': 'text' },{'selector':'img','value':'http://www.lijsoccer.com/images/soccerBall.png'}]));
 
     return widgets;
   }
