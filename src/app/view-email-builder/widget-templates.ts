8604fa1fb038178d4d4890ca6e70f0c6f82bb7bf
Brayden <brayden.h@mynaturalmarket.com>
2017-04-23 13:24:30 -0600

    widget templates in different file, formatted

---
+++ src/app/view-email-builder/widget-templates.ts
@@ -0,0 +1,121 @@
+
+export var template1 = `<table style='width:100%'>
+  <tr align='{{align}}'>
+    <td>{{text1}}</td>
+  </tr>
+</table>`;
+
+export var template2 = `<table style='width:100%'>
+    <tr align='{{align}}'>
+        <td style='width:50%'>
+            {{text1}}
+        </td>
+        <td style='width:50%'>
+            {{text2}}
+        </td>
+    </tr>
+</table>`;
+export var template3 = `<table style='width:100%'>
+	<tr align='{{align}}'>
+		<td style='width:33%'>{{text1}}</td>
+		<td style='width:33%'>{{text2}}</td>
+		<td style='width:33%'>{{text3}}</td>
+	</tr>
+</table>`;
+export var template4 = `<table style='width:100%'>
+	<tr align='{{align}}'>
+		<td>
+			<h1>{{title}}</h1>
+		</td>
+	</tr>
+</table>`;
+export var template5 = `<table style='width:100%'>
+	<tr align='{{align}}'>
+		<td>
+			<h3>{{title}}</h3>
+		</td>
+	</tr>
+</table>`;
+export var template6 = `<table style='width:100%'>
+	<tr align='{{align}}'>
+		<td style='width:100%'><img src='{{img}}' width='200px'></td>
+	</tr>
+</table>`;
+export var template7 = `<table style='width:100%'>
+	<tr align='{{align}}'>
+		<td style='width:50%'><img src='{{img1}}' width='200px'></td>
+		<td style='width:50%'><img src='{{img2}}' width='200px'></td>
+	</tr>
+</table>`;
+export var template8 = `<table style='width:100%'>
+	<tr align='{{align}}'>
+		<td style='width:50%'>
+			<p>{{text}}</p>
+		</td>
+		<td style='width:50%'><img src='{{img}}' width='200px'></td>
+	</tr>
+</table>`;
+export var template9 = `<table style='width:100%'>
+	<tr align='{{align}}'>
+		<td style='width:50%'><img src='{{img}}' width='200px'></td>
+		<td style='width:50%'>
+			<p>{{text}}</p>
+		</td>
+	</tr>
+</table>`;
+export var template10 = `<table style='width:100%'>
+	<tr>
+		<td>
+			<div style='border-bottom:{{color}} 1px solid'></div>
+		</td>
+	</tr>
+</table>`;
+export var template11 = `<table style='width:100%'>
+	<tr>
+		<td>
+			<div style='border-bottom:{{color}} 1px dashed'></div>
+		</td>
+	</tr>
+</table>`;
+export var template12 = `<table style='width:100%'>
+	<tr align='{{align}}'>
+		<td>
+			<a href='{{href}}'>{{linkText}}</a>
+		</td>
+	</tr>
+</table>`;
+
+export var template13 = `<table style='width:100%'>
+	<tr align='{{align}}'>
+		<td style='width:50%'>
+			<a href='{{href1}}'>{{linkText1}}</a>
+		</td>
+		<td style='width:50%'>
+			<a href='{{href2}}'>{{linkText2}}</a>
+		</td>
+	</tr>
+</table>`;
+export var template14 = `<table style='width:100%'>
+	<tr align='{{align}}'>
+		<td>
+			<a href='{{href}}' style='color: white; background-color: {{bgColor1}}; padding: 10px; border-radius: 3px; font-family: Helvetica; font-size: 15px; line-height: 20.8px; word-wrap: break-word; font-weight: normal; text-decoration: none;'>{{linkText}}</a>
+		</td>
+	</tr>
+</table>`;
+export var template15 = `<table style='width:100%'>
+	<tr align='{{align}}'>
+		<td style='width:50%'>
+			<a href='{{href1}}' style='color: white; background-color: {{bgColor1}}; padding: 10px; border-radius: 3px; font-family: Helvetica; font-size: 15px; line-height: 20.8px; word-wrap: break-word; font-weight: normal; text-decoration: none;'>{{linkText1}}</a>
+		</td>
+		<td style='width:50%'>
+			<a href='{{href2}}' style='color: white; background-color: {{bgColor2}}; padding: 10px; border-radius: 3px; font-family: Helvetica; font-size: 15px; line-height: 20.8px; word-wrap: break-word; font-weight: normal; text-decoration: none;'>{{linkText2}}</a>
+		</td>
+	</tr>
+</table>`;
+export var template16 = `<table style='width:100%'>
+	<tr>
+		<td>
+			<div style='height:{{height}}'></div>
+		</td>
+	</tr>
+</table>`;
